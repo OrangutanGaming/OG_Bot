c4c8c2421db27d364adea287e258c279f8e6c619
Nihaal Sangha <nihaal.s@live.co.uk>
2017-02-22 11:19:14 +0000

    Removed IDs

--- BotIDs.py
+++ BotIDs.py
@@ -1,8 +1,8 @@
-clientID = "***REMOVED***"
-clientSecret = "***REMOVED***"
-username = "***REMOVED***"
-token = "***REMOVED***"
-permissionsURL = "***REMOVED***"
+clientID = ""
+clientSecret = ""
+username = ""
+token = ""
+permissionsURL = ""
 OAuth2 = "https://discordapp.com/oauth2/authorize?permissions={}&scope=bot&client_id={}".format(permissionsURL, clientID)
 URL = OAuth2
 print(URL)
