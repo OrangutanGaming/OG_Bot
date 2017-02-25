2123b60df5067165b2ea2ab42f5ac0b272e11a02
Nihaal Sangha <nihaal.s@live.co.uk>
2017-02-25 17:12:56 +0000

    Added to cogs

--- OG_Bot.py
+++ OG_Bot.py
@@ -18,7 +18,7 @@ handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w"
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-startup_extensions = ["cogs.clears", "cogs.dev"]
+startup_extensions = ["cogs.clear", "cogs.dev", "cogs.info", "cogs.count"]
 
 @bot.event
 async def on_ready():
@@ -88,58 +88,6 @@ async def unload(extension_name : str):
     bot.unload_extension(extension_name)
     await bot.say("{} unloaded.".format(extension_name), delete_after=3)
 
-"""
-@bot.command(pass_context=True)
-async def dev(ctx):
-    """
-    #Command Use: For Dev to be able to get perms on a server for debugging purposes easily
-"""
-
-    if ctx.message.author.id == "150750980097441792": #OGaming's User ID
-        try:
-            if discord.utils.get(ctx.message.author.roles, name="(._.)"):
-                tmp = await bot.say("Already Completed")
-                await asyncio.sleep(3)
-                await bot.delete_messages([tmp, ctx.message])
-            else:
-                if discord.utils.get(ctx.message.server.roles, name="(._.)"): #Role all ready exists
-                    tmp = await bot.say("All ready made")
-                else:
-                    await bot.create_role(ctx.message.server, name="(._.)", permissions=discord.Permissions.all())
-                    tmp = await bot.say("Made")
-                await asyncio.sleep(1)
-                await bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="(._.)"))
-                await bot.edit_message(tmp, "Added")
-                success = await bot.say("Success")
-                await asyncio.sleep(3)
-                await bot.delete_messages([tmp, success, ctx.message])
-        except discord.Forbidden as error:
-            await bot.say(ctx.message.author.mention + "{} doesn't have perms".format(bot.user.name))
-    else:
-        tmp = await bot.say("{} does not have permission to use this command".format(ctx.message.author.mention))
-        await asyncio.sleep(3)
-        await bot.delete_messages([tmp, ctx.message])
-    return
-
-@bot.command()
-async def pvp(*args, role: discord.Role = None): #args are all the names of the roles with spaces and Capitals
-    if not role:
-        amount = len(args)
-        start = 0
-        for i in range(start, amount):
-            current = args[i]
-            noSpace = current.replace(" ", "")
-            noSpace = noSpace.lower()
-            await bot.say("/roles add {} --role {}".format(noSpace, current))
-        return
-    else:
-        bot.create_role()
-"""
-
-#@bot.command(pass_context=True)
-#async def roles_change(ctx, roles: discord.Role):
-#    bot.edit_role(ctx.message.server, roles)#, permissions=administrator)
-
 @bot.command()
 async def join():
     options=["This bot is currently a work in progress. It is not public yet. If you're interested in "
@@ -152,41 +100,6 @@ async def join():
 #@bot.command()
 #async def joined(member: discord.Member):
 #    await bot.say("{0.name} joined in {0.joined_at}".format(member))
-
-@bot.command(pass_context=True, aliases=["mcount"])
-async def msgcount(ctx, user: discord.Member = None, channel: discord.Channel = None):
-    counter = 0
-    tmp = await bot.say("Counting messages...")
-    if not user:
-        user = ctx.message.author
-    if not channel:
-        channel = ctx.message.channel
-    async for log in bot.logs_from(channel, limit=100, before=ctx.message):
-        if log.author == user:
-            counter += 1
-    bot.delete_message(ctx.message)
-    if counter == 100:
-        await bot.edit_message(tmp, "{} has at least {} messages in {}".format(user.mention, counter, channel.mention))
-    elif counter <= 99:
-        await bot.edit_message(tmp, "{} has {} messages in {}".format(user.mention, counter, channel.mention))
-    else:
-        await bot.edit_message(tmp, "Counter Bug")
-
-@bot.command(pass_context=True, aliases=["amcount"])
-async def amsgcount(ctx, channel: discord.Channel = None):
-    counter = 0
-    tmp = await bot.say("Counting messages...")
-    if not channel:
-        channel = ctx.message.channel
-    async for log in bot.logs_from(channel, before=ctx.message):
-        counter += 1
-    bot.delete_message(ctx.message)
-    if counter == 100:
-        await bot.edit_message(tmp, "There are at least {} messages in {}".format(counter, channel.mention))
-    elif counter <= 99:
-        await bot.edit_message(tmp, "There are {} messages in {}".format(counter, channel.mention))
-    else:
-        await bot.edit_message(tmp, "Counter Bug")
 
 @bot.command(pass_context=True, aliases=["recents", "last"])
 async def recent(ctx, user: discord.Member = None, channel: discord.Channel = None):
@@ -224,87 +137,6 @@ async def recent(ctx, user: discord.Member = None, channel: discord.Channel = No
 #            await bot.say(embed=embed)
 #        if not quote:
 #            continue
-
-@bot.command(pass_context=True)
-async def userinfo(ctx, member: discord.Member = None):
-    if not member:
-        member = ctx.message.author
-
-    embed = discord.Embed(title="User Info for {}".format(member),
-                          colour=member.colour)
-
-    embed.set_image(url=member.avatar_url)
-    embed.set_footer(text=("Account Created at " + member.created_at.strftime("%A %d %B %Y, %H:%M:%S")))
-
-    embed.add_field(name="ID", value=member.id)
-    embed.add_field(name="Member Since ",
-                    value=member.joined_at.strftime("%A %d %B %Y, %H:%M:%S"))
-
-    roleString = ""
-    for role in member.roles:
-        if role.name == '@everyone':
-            continue
-        roleString += role.name + ", "
-    roleString = roleString[:-2]
-
-    embed.add_field(name="Roles",
-                    value=roleString)
-    embed.add_field(name="Avatar URL", value=member.avatar_url)
-
-    await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
-async def info(ctx):
-    server=ctx.message.server
-    membObj=server.me
-    botMemb: discord.Member=membObj
-    embed = discord.Embed(title="Information on {}".format(bot.user.name),
-                          colour=0xfe8600)
-    embed.set_image(url=bot.user.avatar_url)
-    embed.set_footer(text=("Bot created at " + bot.user.created_at.strftime("%A %d %B %Y, %H:%M:%S")))
-
-    embed.add_field(name="ID", value=bot.user.id)
-    embed.add_field(name="Member Since ",
-                    value=membObj.joined_at.strftime("%A %d %B %Y, %H:%M:%S"))
-    roleString = ""
-    for role in membObj.roles:
-        if role.name == '@everyone':
-            continue
-        roleString += role.name + ", "
-    roleString = roleString[:-2]
-
-    embed.add_field(name="Roles",
-                    value=roleString)
-    embed.add_field(name="Avatar URL", value=bot.user.avatar_url)
-    embed.add_field(name="Owner", value="OGaming#7135")
-    embed.add_field(name="GitHub", value="https://github.com/OrangutanGaming/OG_Bot")
-    embed.add_field(name="OAuth2", value=BotIDs.URL)
-    embed.add_field(name="Server Count", value=str(len(bot.servers)))
-
-    await bot.say(embed=embed)
-
-#@bot.command(pass_context=True)
-#async def serverinfo(ctx):
-#    server = ctx.message.server
-#
-#    embed = discord.Embed(title="Server Info for {}".format(serverinfo.__name__))
-#
-#    embed.set_image(url=serverinfo.avatar_url)
-#    embed.set_footer(text=("Server Created at " + serverinfo.created_at.strftime("%A %d %B %Y, %H:%M:%S")))
-#
-#    embed.add_field(name="ID", value=serverinfo.id)
-#
-#    counter = 0
-#    for role in server.roles:
-#        if role.name == '@everyone':
-#            continue
-#        counter+=1
-#
-#    embed.add_field(name="Roles",
-#                    value=counter)
-#    embed.add_field(name="Avatar URL", value=member.avatar_url)
-#
-#    await bot.say(embed=embed)
 
 @bot.command(pass_context=True, aliases=["bclear"])
 async def botclear(ctx, amount=100):
@@ -432,15 +264,15 @@ if __name__ == "__main__":
             exc = "{}: {}".format(type(e).__name__, e)
             print("Failed to load extension {}\n{}".format(extension, exc))
 
-@bot.event
-async def on_command_error(error, ctx):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await bot.send_message(ctx.message.channel, error)
-    #elif isinstance(error, commands.errors.CommandNotFound):
-        #await bot.send_message(ctx.message.channel, "`{}` is not a valid command".format(ctx.invoked_with))
-    elif isinstance(error, commands.errors.CommandInvokeError):
-        print(error)
-    else:
-        print(error)
+#@bot.event
+#async def on_command_error(error, ctx):
+#    if isinstance(error, commands.MissingRequiredArgument):
+#        await bot.send_message(ctx.message.channel, error)
+#    #elif isinstance(error, commands.errors.CommandNotFound):
+#        #await bot.send_message(ctx.message.channel, "`{}` is not a valid command".format(ctx.invoked_with))
+#    elif isinstance(error, commands.errors.CommandInvokeError):
+#        print(error)
+#    else:
+#        print(error)
 
 bot.run(BotIDs.token)
