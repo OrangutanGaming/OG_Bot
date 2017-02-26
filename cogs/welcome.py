dd46db65b0f84cc0d713920087b19ccff24fa076
Nihaal Sangha <nihaal.s@live.co.uk>
2017-02-26 10:20:54 +0000

    Fixed Message not getting sent

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -5,8 +5,8 @@ class Welcome():
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_member_join(self, member):
-        await self.bot.say("{0.name} joined in {0.joined_at}".format(member))
+    async def on_member_join(self, ctx, member):
+        await self.bot.send_message(ctx.message.server.defaukt_channel, "{0.name} joined in {0.joined_at}".format(member))
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
