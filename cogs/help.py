d507464c98ecb84d962265669c9fa4f33579ba29
Nihaal Sangha <nihaal.s@live.co.uk>
2017-02-28 18:59:22 +0000

    Moved help to cogs

---
+++ cogs/help.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+import discord
+
+class Help():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def help(self, ctx):
+        await ctx.send("OG_Bot by Orangutan Gaming `(OGaming#7135)`"
+                    "\nPrefixes:`?`"
+                    "\n`<Mandatory Argument>`, `(Optional Argument)` `Alias 1`/`Alias 2` [Permission Needed]"
+                    "\n`help`: Gives this command"
+                    "\n`join`: Shows information on how to add me to your server"
+                    "\n`msgcount`/`mcount` (user) (channel): Count how many messages in the channel the command was used "
+                    "in or the channel given by the user given. If no user is given, it will use the person who uses the "
+                    "command"
+                    "\n`amsgcount`/`amcount` (channel): Count how many messages in the channel given. If no channel was"
+                    "given, will use the channel the command was used in"
+                    "\n`recent`/`recents`/`last` (user) (channel): Quotes the most recent message from the user and channel given."
+                    "If no user is given, it will use the user using the command and if no channel is given it will use"
+                    "the channel the command was used in."
+                    "\n`userinfo` (user) gets the userinfo of the user given. If no user is given, it will use the"
+                    "user using the command"
+                    "\n`info`: Displays Bot Info"
+                    #"\n`botclear`/`bclear` (amount) [Manage Messages]: Deletes the amount of messages given by me in the "
+                    #"current channel. Default: 100"
+                    "\n`clear`/`del`/`delete`/`wipe` (amount) [Manage Messages]: Deletes the amount of messages given in "
+                    "the current channel. Default: 100"
+                    "\n`stickers`: Displays all the availbile stickers"
+                    "\n`github`: Displays GitHub Link"
+                    "\n`support`: Displays Patreon Link")
+
+def setup(bot):
+    bot.add_cog(Help(bot))
