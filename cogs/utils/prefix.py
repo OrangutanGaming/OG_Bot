c72fcda37dd31d80941fde10fa14adf408b25ca2
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-06 21:19:18 +0000

    Added Prefix

---
+++ cogs/utils/prefix.py
@@ -0,0 +1,10 @@
+prefixes = ["?", "!.o"]
+
+def Prefix(quote = None):
+    if quote == None:
+        quote = '"'
+    pPrefix = ""
+    for prefix in prefixes:
+        pPrefix += ('{}'.format(quote)+prefix+'{}'.format(quote)+", ")
+    pPrefix = pPrefix[:-2]
+    return pPrefix
