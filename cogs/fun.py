f3c39ae6e2668a27e4a28ccefb7dd7e754e04a5c
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-11 10:21:28 +0000

    made o!ping neater

--- cogs/fun.py
+++ cogs/fun.py
@@ -7,15 +7,13 @@ class Fun():
 
     @commands.command()
     async def ping(self, ctx):
-        pong = await ctx.send("Pong. :ping_pong:")
-
         before = time.monotonic()
         await (await self.bot.ws.ping())
         after = time.monotonic()
         pingT = (after - before) * 1000
-        pingT = str(int(pingT))
+        pingT = round(pingT)
 
-        await pong.edit(content="Pong. :ping_pong: **{}ms**".format(pingT))
+        await ctx.send("Pong. :ping_pong: **{}ms**".format(pingT))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
