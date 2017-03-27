788866d0ce5b63cbfc61efa948430e59f165f27e
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 17:27:27 +0100

    Added Servercount command

--- cogs/help.py
+++ cogs/help.py
@@ -8,7 +8,7 @@ class Help():
     @commands.command()
     async def help(self, ctx):
         await ctx.send("OG_Bot by Orangutan Gaming `(OGaming#7135)`"
-                    "\nPrefixes: `@OG_Bot`, {}".format(Prefix.Prefix('`')) +
+                    f"\nPrefixes: `@OG_Bot`, {Prefix.Prefix('`')}"
                     "\n`<Mandatory Argument>`, `(Optional Argument)` `Alias 1`/`Alias 2` `[Permission Needed]`"
                     "\n`help`: Shows this message"
                     "\n`join`: Shows information on how to add me to your server"
@@ -23,11 +23,11 @@ class Help():
                     "\n`userinfo` (user) gets the userinfo of the user given. If no user is given, it will use the"
                     "user using the command"
                     "\n`info`/`uinfo`: Displays Bot Info"
-                    #"\n`channelinfo`/`cinfo` (channel): Displays channel's info. If none given, will use current channel"
                     #"\n`botclear`/`bclear` (amount) [Manage Messages]: Deletes the amount of messages given by me in the "
                     #"current channel. Default: 100"
                     "\n`clear`/`del`/`delete`/`wipe` (amount) [Manage Messages]: Deletes the amount of messages given in "
                     "the current channel. Default: 100"
+                    "\n`charinfo` <Emoji>: Gives the info for an emoji"
                     "\n`stickers`: Displays all the available stickers"
                     "\n`github`: Displays GitHub Link"
                     "\n`support`: Displays Patreon Link"
@@ -36,6 +36,10 @@ class Help():
     @commands.command()
     async def prefix(self, ctx):
         await ctx.send("Prefixes: `@OG_Bot`, " + Prefix.Prefix('`'))
+
+    @commands.command(aliases=["scount"])
+    async def servercount(self, ctx):
+        await ctx.send("Server Count: " + str(len(self.bot.guilds)))
 
 def setup(bot):
     bot.add_cog(Help(bot))
