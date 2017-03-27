f8aa2fe77e38ad44b3ebc295428053195c710da1
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 17:27:01 +0100

    Added Charinfo

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import time
+import unicodedata
 
 class Fun():
     def __init__(self, bot):
@@ -14,6 +15,22 @@ class Fun():
         pingT = round(pingT)
 
         await ctx.send("Pong. :ping_pong: **{}ms**".format(pingT))
+
+    @commands.command()
+    async def charinfo(self, ctx, *, characters: str):
+
+        if len(characters) > 15:
+            await ctx.send(f"Too many characters ({len(characters)}/15)")
+            return
+
+        fmt = "`\\U{0:>08}`: {1} - {2} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{0}>"
+
+        def to_string(c):
+            digit = format(ord(c), "x")
+            name = unicodedata.name(c, "Name not found.")
+            return fmt.format(digit, name, c)
+
+        await ctx.send("\n".join(map(to_string, characters)))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
