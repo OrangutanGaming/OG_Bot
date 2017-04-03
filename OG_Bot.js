72afa88733379179258a74516716cd19706c0247
Nihaal Sangha <nihaal.s@live.co.uk>
2017-04-03 17:42:50 +0100

    Added Basic Features

--- OG_Bot.js
+++ OG_Bot.js
@@ -1,28 +1,36 @@
 const Discord = require("discord.js");
 const Colours = require("colors");
 const BotIDs = require("./BotIDs.js");
+const Raven = require("raven");
+
+const client = Raven.config(BotIDs.ravenURL);
+client.install();
+const Logger = require("./Logger.js");
+const logger = new Logger(client);
 
 const bot = new Discord.Client();
 const token = BotIDs.token;
 
 const game = "with OG|o!help";
 const prefixes = ["o!", "o."];
-const prefix = "o!"
+const prefix = "o.";
 
 bot.on("ready", () => {
-    /*
-     @bot.event
-     async def on_ready():
-     print("Prefixes: " + Prefixes.Prefix('"'))
-     */
     console.log("Logged in as:");
     console.log(`Name: ${bot.user.username}#${bot.user.discriminator}`);
     console.log("ID: " + bot.user.id);
     bot.user.setGame(game);
     console.log("Playing".bold, game);
     console.log(BotIDs.OAuth2);
-    console.log(`Prefixes: ${prefixes.join(", ")}`);
+    console.log(`Prefixes: ${prefixes.join(", ")}`); // Not True
+    // TODO Multiple Prefixes
 
+});
+
+bot.on("guildCreate", guild => {
+    try {guild.defaultChannel.sendMessage("Welcome to the world of Orangutans! I was made by `OGaming#7135` Run `o!help` " +
+        "for help");}
+        catch (e) {return}
 });
 
 bot.on("message", message => {
@@ -30,11 +38,28 @@ bot.on("message", message => {
     if (!message.content.startsWith(prefix)) return;
 
     let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length)
+    command = command.slice(prefix.length);
+
+    let args = message.content.split(" ").slice(1);
+
+    if (command === "add") {
+        let numArray = args.map(n => parseInt(n));
+        let total = numArray.reduce((p, c) => p+c);
+
+        Args = args.join(" + ");
+        Args = `\`${Args}\``;
+
+        message.channel.sendMessage(`${Args} is \`${total}\``)
+    }
 
     if (command === "ping") {
         message.channel.sendMessage("pong");
     }
+
+    if (command === "uptime") {
+        message.channel.sendMessage("uptime: " + bot.uptime)
+    }
+
 });
 
 bot.login(token);
