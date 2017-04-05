3b96584e48426c9dcc801ad6135d95ca0af3292a
Nihaal Sangha <nihaal.s@live.co.uk>
2017-04-05 16:01:07 +0100

    Changed logging level

--- OG_Bot.py
+++ OG_Bot.py
@@ -13,7 +13,7 @@ import datetime
 bot = commands.Bot(command_prefix="?", description="Orangutan Gaming's bot")
 
 logger = logging.getLogger("discord")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
