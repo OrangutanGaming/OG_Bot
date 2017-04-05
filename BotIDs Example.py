108d0e42b7deab7cb63df383e852b5ca13c06f4a
Nihaal Sangha <nihaal.s@live.co.uk>
2017-04-05 16:00:49 +0100

    Added BotIDs example

---
+++ BotIDs Example.py
@@ -0,0 +1,12 @@
+clientID = ""
+clientSecret = ""
+username = ""
+token = ""
+
+permissionsURL = "2146958463" # https://finitereality.github.io/permissions/?v=0
+OAuth2 = "https://discordapp.com/oauth2/authorize?permissions={}&scope=bot&client_id={}".format(permissionsURL, clientID)
+URL = OAuth2
+
+GitHubUser = "OrangutanGaming"
+GitHubRepo = "OG_Bot.py"
+GitHub = "https://github.com/{}/{}".format(GitHubUser, GitHubRepo)
