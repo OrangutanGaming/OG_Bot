df61b6e29e862b74bcca1872567d243e7edb959c
Nihaal Sangha <nihaal.s@live.co.uk>
2017-04-05 17:53:35 +0100

    Updated BotIDs system

--- BotIDs.py
+++ BotIDs.py
@@ -1,8 +1,24 @@
-clientID = ""
-clientSecret = ""
-username = ""
-token = ""
-permissionsURL = ""
+import json
+import os
+
+dir = os.path.dirname(os.path.abspath(__file__))
+with open(f"{dir}/settings.json") as data_file:
+    settings = json.load(data_file)
+
+clientID = settings["clientID"]
+token = settings["token"]
+owner = settings["ownerID"]
+
+dev_id = 150750980097441792
+permissionsURL = "2146958463" # https://finitereality.github.io/permissions/
 OAuth2 = "https://discordapp.com/oauth2/authorize?permissions={}&scope=bot&client_id={}".format(permissionsURL, clientID)
 URL = OAuth2
-print(URL)
+
+GitHubUser = "OrangutanGaming"
+GitHubRepo = "OG_Bot.py"
+GitHub = "https://github.com/{}/{}".format(GitHubUser, GitHubRepo)
+
+Patreon = "https://www.patreon.com/OrangutanGaming"
+
+bots_discord_pw_key = settings["bots_discord_pw"]
+discord_pw = bots_discord_pw_key
