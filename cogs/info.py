b18ff01a9551b505b898e92cce26c7d3c8b5f914
Nihaal Sangha <nihaal.s@live.co.uk>
2017-04-05 17:58:44 +0100

    Added more to o!sinfo

--- cogs/info.py
+++ cogs/info.py
@@ -104,6 +104,9 @@ class Info():
         embed.add_field(name="Region", value=server.region)
         embed.add_field(name="Member Count", value=server.member_count)
         embed.add_field(name="Bot Count", value=Bots(server))
+        embed.add_field(name="Text Channel Count", value=str(len(server.text_channels)))
+        embed.add_field(name="Voice Channel Count", value=str(len(server.voice_channels)))
+        embed.add_field(name="Total Channel Count", value=str(len(server.channels)))
         if server.icon_url:
             embed.set_image(url=server.icon_url)
             embed.add_field(name="Avatar URL", value=server.icon_url)
